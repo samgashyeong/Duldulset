59b41868f233637337eea7f9df04218174f43a49
이준상 <66434787+samgashyeong@users.noreply.github.com>
2025-11-19 13:17:21 +0900

    test 파일

--- Script/testChar1.gd
+++ Script/testChar1.gd
@@ -1,31 +1,27 @@
 extends Sprite2D
 
 @export var playerType : Type.StaffName
-
 var mouseEnter = false
-var newbox
+var newbox = null
 func _ready() -> void:
-	var timer = get_tree().create_timer(2.0)
+	var timer = get_tree().create_timer(3.0)
 
 
 	await timer.timeout
 	newbox = BubbleManager.startDialog(global_position, Type.StaffName.JUNSANG)
 	newbox.textToDisPlay(Type.StaffMethod.ORDER)
-	
+
 
 func _unhandled_input(event: InputEvent) -> void:
 	if event.is_action_pressed("inter_action") and mouseEnter:
-		print("fffasdf")
-		newbox.textToDisPlay(Type.StaffMethod.START2)
+		##여기에 원래 interActionWithPlayer를 호출하려고 했지만 싱글톤 패턴때문에 고민중임
+		print("asdf")
+		newbox.textToDisPlay(Type.StaffMethod.START0)
 
 
-func _on_area_2d_mouse_entered() -> void:
+func _on_area_2d_2_mouse_entered() -> void:
 	mouseEnter = true
-	print(mouseEnter)
 
 
-
-
-func _on_area_2d_mouse_exited() -> void:
+func _on_area_2d_2_mouse_exited() -> void:
 	mouseEnter = false
-	print(mouseEnter)
---
+++ Script/testrrr.gd
@@ -0,0 +1,9 @@
+extends Node
+class_name testPlayer
+
+var mouseEnter = false
+
+
+
+func _on_area_2d_3_mouse_entered() -> void:
+	mouseEnter = true
