a3385d6c84183555d3281214210ae20ad23f1eff
Lee Jun Sang <66434787+samgashyeong@users.noreply.github.com>
2025-11-29 20:53:00 +0900

    각 스페셜 이벤트에 대한 다이얼로그 완성

---
+++ Script/DialogicSignalHandler.gd
@@ -0,0 +1,122 @@
+extends Node
+
+var attendance_timer: Timer
+var intro_timer: Timer
+var current_choice_timeout: float = 1.5  # 5 second attendance timer
+var intro_timeout: float = 3.0  # 3 second intro timer
+var is_waiting_for_choice: bool = false
+
+# order manager
+var attendance_order = ["junsang", "dongwoo", "yeonghee", "minseo", "giyeong"]
+var current_student_index = 0
+
+func _ready():
+	Dialogic.signal_event.connect(_on_dialogic_signal)
+	
+	attendance_timer = Timer.new()
+	attendance_timer.wait_time = current_choice_timeout
+	attendance_timer.one_shot = true
+	attendance_timer.timeout.connect(_on_attendance_timer_timeout)
+	add_child(attendance_timer)
+	
+	#inter timer setting
+	intro_timer = Timer.new()
+	intro_timer.wait_time = intro_timeout
+	intro_timer.one_shot = true
+	intro_timer.timeout.connect(_on_intro_timer_timeout)
+	add_child(intro_timer)
+
+signal effectHealth()
+signal effectStamia()
+signal effectPoint()
+
+enum SpeicalName{
+	Teemu,
+	Sinyeoung,
+	Jenson
+}
+
+func _on_dialogic_signal(argument: Variant):
+	print("Received signal with argument: ", argument)
+	
+	#attendance check signal manager
+	if argument == "start_intro_timer":
+		start_intro_timer()
+	elif argument == "start_attendance_timer":
+		start_attendance_timer()
+	elif argument == "attendance_finished":
+		attendance_finished()
+	elif argument == "stop_intro_timer":
+		stop_intro_timer()
+	elif argument == "stop_timer":
+		stop_timer()
+	
+	if argument is Dictionary:
+		var name = argument.get("Name")
+		print(name)
+		match name:
+			"Teemu":
+				teemuEvent()
+			"Sinyeoung":
+				sinyeoungEvent()
+			"Jenson":
+				JensonEvent()
+
+#attendence
+func start_attendance_timer():
+	is_waiting_for_choice = true
+	attendance_timer.start()
+
+func _on_attendance_timer_timeout():
+	print("timeout")
+	if is_waiting_for_choice:
+		is_waiting_for_choice = false
+		proceed_to_next_student()
+
+func proceed_to_next_student():
+	current_student_index += 1
+	
+	if current_student_index < attendance_order.size():
+		var next_student = attendance_order[current_student_index]
+		Dialogic.Jump.jump_to_label(next_student)
+		Dialogic.handle_next_event()
+	else:
+		print("end")
+		if Dialogic.Jump:
+			Dialogic.Jump.jump_to_label("end")
+			Dialogic.handle_next_event()
+
+func attendance_finished():
+	is_waiting_for_choice = false
+	attendance_timer.stop()
+	Dialogic.end_timeline()
+
+func start_intro_timer():
+	print("인트로 타이머 시작")
+	intro_timer.start()
+
+				
+func _on_intro_timer_timeout():
+	if Dialogic.current_timeline:
+		Dialogic.Jump.jump_to_label("junsang")
+		Dialogic.handle_next_event()
+	current_student_index = 0
+
+func stop_intro_timer():
+	is_waiting_for_choice = false
+	intro_timer.stop()
+func stop_timer():
+	is_waiting_for_choice = false
+	attendance_timer.stop()
+				
+func teemuEvent():
+	print("Ok")
+	print("Event")
+
+func sinyeoungEvent():
+	print("Ok")
+	print("Event")
+
+func JensonEvent():
+	print("Ok")
+	print("Event")
