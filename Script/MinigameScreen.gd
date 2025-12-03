ab9812bd53bc5980b99c06ea53ca2429a998648b
RaCuNi <thisisracuni@gmail.com>
2025-12-03 13:14:35 +0900

    Add minigame script for managing minigames.

--- Script/MinigameScreen.gd
+++ Script/MinigameScreen.gd
@@ -1,11 +1,25 @@
-extends SubViewportContainer
+extends Node
 
-@onready var coffee_machine = $"../Map/CoffeeMaker"
+var minigame_manager
+var player_computer
+var copy_machine
 
 func _ready():
-	hide()
-	coffee_machine.coffee_making.connect(_on_coffee_machine_coffee_making)
+	minigame_manager = $"MiniGameManager"
+	minigame_manager.minigame_closed.connect(_on_minigame_manager_minigame_closed)
 	
+	player_computer = $"../Map/Tables/Part/PlayerDesk"
+	player_computer.using_computer.connect(_on_player_computer_using_computer)
+	
+	copy_machine = $"../Map/Copy"
+	copy_machine.using_copy_machine.connect(_on_copy_machine_using_copy_machine)
 
-func _on_coffee_machine_coffee_making():
-	show()
+
+func _on_minigame_manager_minigame_closed(success: bool):
+	GameData.is_playing_minigame = false
+	
+func _on_player_computer_using_computer():
+	minigame_manager.open_minigame(0)
+
+func _on_copy_machine_using_copy_machine():
+	minigame_manager.open_minigame(1)
