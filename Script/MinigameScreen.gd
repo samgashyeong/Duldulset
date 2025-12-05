dd85d74944d68ffa13ed84572bb3208311b719bd
RaCuNi <thisisracuni@gmail.com>
2025-12-05 18:57:42 +0900

    Update MinigameScreen.gd for checking minigame names.

--- Script/MinigameScreen.gd
+++ Script/MinigameScreen.gd
@@ -4,20 +4,27 @@ var minigame_manager
 var player_computer
 var copy_machine
 
+var current_minigame: String
+
 func _ready():
 	minigame_manager = $MiniGameManager
 	minigame_manager.minigame_closed.connect(_on_minigame_manager_minigame_closed)
+	minigame_manager.minigame_shown.connect(_on_minigame_manager_minigame_shown)
 	
 	player_computer = $"../Map/Tables/Part/PlayerDesk"
 	player_computer.using_computer.connect(_on_player_computer_using_computer)
 
 	copy_machine = $"../Map/Copy"
 	copy_machine.using_copy_machine.connect(_on_copy_machine_using_copy_machine)
-
+	
+	current_minigame = ""
 
 func _on_minigame_manager_minigame_closed(success: bool):
 	GameData.is_playing_minigame = false
 
+func _on_minigame_manager_minigame_shown(game_name: String):
+	current_minigame = game_name
+	
 func _on_player_computer_using_computer():
 	minigame_manager.open_minigame(0)
 
